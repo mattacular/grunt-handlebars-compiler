2960b5adcdef33e8186782eea2c86b46723e296e
pasang sherpa <pasang_sh@hotmail.com>
2014-06-19 14:58:52 -0700

    fix for returning templates

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -6,164 +6,167 @@
  *	Licensed under the MIT license.
  */
 module.exports = function(grunt) {
-	'use strict';
-	
-	var _ = grunt.util._,
-		uglify = require('uglify-js'),
-		defaultProcessFilename;
+    'use strict';
 
-	// filename conversion for templates and partials
-	defaultProcessFilename = function (filePath) {
-		var pieces = _.last(filePath.split('/')).split('.'),		// strips off path and gets the filename
-			name   = _(pieces).without(_.last(pieces)).join('-');	// strips file extension
+    var _ = grunt.util._,
+        uglify = require('uglify-js'),
+        defaultProcessFilename;
 
-		return name;
-	};
+    // filename conversion for templates and partials
+    defaultProcessFilename = function(filePath) {
+        var pieces = _.last(filePath.split('/')).split('.'), // strips off path and gets the filename
+            name = _(pieces).without(_.last(pieces)).join('-'); // strips file extension
 
-	// the 'handlebars' task
-	grunt.registerMultiTask('handlebars', 'Compile *.handlebars templates with given options.', function () {
-		var options = this.options({
-				separator: grunt.util.linefeed,
-				namespace: 'Handlebars.templates',
-				exportAMD: false,
-				exportCommonJS: false,
-				pathToHandlebars: '',		// only relevant to 'exportAMD: true' - amd style option
-				knownHelpers: [],			// provide an array of known helpers
-				knownOnly: false,			// compile known helpers only
-				templateRoot: false,		// base value to strip from template names
-				partial: false,				// specify that templates are partials
-				min: false					// minify output
-			}),
-			compilerOptions = {},
-			known = {},
-			knownLabel = '',
-			processFilename,
-			prefix, midfix, wrapOpen, wrapClose, suffix;
+        return name;
+    };
 
-		grunt.verbose.writeflags(options, 'Options');
+    // the 'handlebars' task
+    grunt.registerMultiTask('handlebars', 'Compile *.handlebars templates with given options.', function() {
+        var options = this.options({
+            separator: grunt.util.linefeed,
+            namespace: 'Handlebars.templates',
+            exportAMD: false,
+            exportCommonJS: false,
+            pathToHandlebars: '', // only relevant to 'exportAMD: true' - amd style option
+            knownHelpers: [], // provide an array of known helpers
+            knownOnly: false, // compile known helpers only
+            templateRoot: false, // base value to strip from template names
+            partial: false, // specify that templates are partials
+            min: false, // minify output
+            returnTemplates: false // retrun templates
+        }),
+            compilerOptions = {},
+            known = {},
+            knownLabel = '',
+            processFilename,
+            prefix, midfix, wrapOpen, wrapClose, suffix;
 
-		// check for valid knownHelpers option
-		if (!Array.isArray(options.knownHelpers)) {
-			options.knownHelpers = [];
-		} else {
-			compilerOptions.knownHelpers = options.knownHelpers;
-		}
+        grunt.verbose.writeflags(options, 'Options');
 
-		// decide template prefix & suffix
-		if (options.exportAMD && options.exportCommonJS) {
-			grunt.fail.warn('Cannot choose to compile as both an AMD and a CommonJS module. Please remove either the \'exportAMD\' or \'exportCommonJS\' option from your Gruntfile.js.');
-		} else if (options.exportAMD) {
-			prefix = 'define([\'' + options.pathToHandlebars + 'handlebars\'], function (Handlebars) {\n';
-			grunt.log.writeln('Compiling as AMD/RequireJS module(s).');
-			suffix = '});';
-		} else if (options.exportCommonJS) {
-			if (typeof options.exportCommonJS !== 'string') {
-				grunt.fail.warn('Must provide a path to Handlebars module in order to compile as a CommonJS module.');
-			}
-			grunt.log.writeln('Compiling as Common JS module(s).');
-			prefix = 'var Handlebars = require(\'' + options.exportCommonJS + '\');\n';
-			suffix = '';
-		} else {
-			prefix = '(function() {\n';
-			suffix = '}());';
-		}
+        // check for valid knownHelpers option
+        if (!Array.isArray(options.knownHelpers)) {
+            options.knownHelpers = [];
+        } else {
+            compilerOptions.knownHelpers = options.knownHelpers;
+        }
 
-		// decide template midfix
-		midfix = 'var template = Handlebars.template, templates = ' + options.namespace + ' = ' + options.namespace + ' || {};\n';
+        // decide template prefix & suffix
+        if (options.exportAMD && options.exportCommonJS) {
+            grunt.fail.warn('Cannot choose to compile as both an AMD and a CommonJS module. Please remove either the \'exportAMD\' or \'exportCommonJS\' option from your Gruntfile.js.');
+        } else if (options.exportAMD) {
+            prefix = 'define([\'' + options.pathToHandlebars + 'handlebars\'], function (Handlebars) {\n';
+            grunt.log.writeln('Compiling as AMD/RequireJS module(s).');
+            suffix = options.returnTemplates ? 'return templates;});' : '});';
+        } else if (options.exportCommonJS) {
+            if (typeof options.exportCommonJS !== 'string') {
+                grunt.fail.warn('Must provide a path to Handlebars module in order to compile as a CommonJS module.');
+            }
+            grunt.log.writeln('Compiling as Common JS module(s).');
+            prefix = 'var Handlebars = require(\'' + options.exportCommonJS + '\');\n';
+            suffix = options.returnTemplates ? 'return templates' : '';
+        } else {
+            prefix = '(function() {\n';
+            suffix = options.returnTemplates ? 'return templates;}());' : '}());';
+        }
 
-		// assign filename processing (this decides template name under namespace)
-		processFilename = options.processFilename || defaultProcessFilename;
+        // decide template midfix
+        midfix = 'var template = Handlebars.template, templates = ' + options.namespace + ' = ' + options.namespace + ' || {};\n';
 
-		// convert known helpers array/list to an object literal
-		if (options.knownHelpers && !Array.isArray(options.knownHelpers)) {
-			options.knownHelpers = [options.knownHelpers];
-		}
+        // assign filename processing (this decides template name under namespace)
+        processFilename = options.processFilename || defaultProcessFilename;
 
-		if (options.knownHelpers && options.knownHelpers.length > 0) {
-			for (var i = 0; i < options.knownHelpers.length; i += 1) {
-				known[options.knownHelpers[i]] = true;
-				knownLabel += (i !== options.knownHelpers.length - 1) ? options.knownHelpers[i] + ', ' : options.knownHelpers[i];
-			}
+        // convert known helpers array/list to an object literal
+        if (options.knownHelpers && !Array.isArray(options.knownHelpers)) {
+            options.knownHelpers = [options.knownHelpers];
+        }
 
-			grunt.log.writeln('Compiling with known helpers: ');
-			grunt.log.ok(knownLabel);
+        if (options.knownHelpers && options.knownHelpers.length > 0) {
+            for (var i = 0; i < options.knownHelpers.length; i += 1) {
+                known[options.knownHelpers[i]] = true;
+                knownLabel += (i !== options.knownHelpers.length - 1) ? options.knownHelpers[i] + ', ' : options.knownHelpers[i];
+            }
 
-			compilerOptions = {
-				'knownHelpers': known,
-				'knownHelpersOnly': options.knownOnly
-			};
-		}
+            grunt.log.writeln('Compiling with known helpers: ');
+            grunt.log.ok(knownLabel);
 
-		this.files.forEach(function (f) {
-			var partials = [],
-				templates = [],
-				output;
+            compilerOptions = {
+                'knownHelpers': known,
+                'knownHelpersOnly': options.knownOnly
+            };
+        }
 
-			// iterate files, processing partials and templates separately
-			f.src.filter(function (filepath) {
-				// Warn on and remove invalid source files (if nonull was set).
-				if (!grunt.file.exists(filepath)) {
-					grunt.log.warn('Source file "' + filepath + '" not found.');
-					return false;
-				} else {
-					return true;
-				}
-			})
-			.forEach(function (filepath) {
-				var src = grunt.file.read(filepath),
-					compiled, filename;
+        this.files.forEach(function(f) {
+            var partials = [],
+                templates = [],
+                output;
 
-				filepath = filepath.split('/');
-				filename = _.last(filepath);
+            // iterate files, processing partials and templates separately
+            f.src.filter(function(filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            })
+                .forEach(function(filepath) {
+                    var src = grunt.file.read(filepath),
+                        compiled, filename;
 
-				// option: need to check for templateRoot to strip?
-				if (options.templateRoot && filename.indexOf(options.templateRoot) === 0) {
-					filename = filename.replace(options.templateRoot, '');
-					filepath[(filepath.length - 1)] = filename;
-					grunt.log.writeln('Found templateRoot and stripped it from the template name: "' + filename + '"');
-				}
+                    filepath = filepath.split('/');
+                    filename = _.last(filepath);
 
-				filepath = filepath.join('/');
-				filename = processFilename(filepath);
+                    // option: need to check for templateRoot to strip?
+                    if (options.templateRoot && filename.indexOf(options.templateRoot) === 0) {
+                        filename = filename.replace(options.templateRoot, '');
+                        filepath[(filepath.length - 1)] = filename;
+                        grunt.log.writeln('Found templateRoot and stripped it from the template name: "' + filename + '"');
+                    }
 
-				try {
-					compiled = require('handlebars').precompile(src, compilerOptions);
-				} catch (e) {
-					grunt.log.error(e);
-					grunt.fail.warn('Handlebars failed to compile '+filepath+'.');
-				}
+                    filepath = filepath.join('/');
+                    filename = processFilename(filepath);
 
-				// source is compiled at this point, let us reconstruct and decide wrappings
-				if (options.partials) {
-					wrapOpen = 'Handlebars.partials[\'' + filename + '\'] = template(';
-				} else {
-					wrapOpen = 'templates[\'' + filename + '\'] = template(';
-				}
+                    try {
+                        compiled = require('handlebars').precompile(src, compilerOptions);
+                    } catch (e) {
+                        grunt.log.error(e);
+                        grunt.fail.warn('Handlebars failed to compile ' + filepath + '.');
+                    }
 
-				wrapClose = ');\n';
+                    // source is compiled at this point, let us reconstruct and decide wrappings
+                    if (options.partials) {
+                        wrapOpen = 'Handlebars.partials[\'' + filename + '\'] = template(';
+                    } else {
+                        wrapOpen = 'templates[\'' + filename + '\'] = template(';
+                    }
 
-				// finally, put it all back together
-				compiled = wrapOpen + compiled + wrapClose;
+                    wrapClose = ');\n';
 
-				templates.push(compiled);
-			});
-			
-			templates = templates.join(grunt.util.normalizelf(options.separator));
+                    // finally, put it all back together
+                    compiled = wrapOpen + compiled + wrapClose;
 
-			var wrappedTemplates = prefix + midfix + templates + suffix;
+                    templates.push(compiled);
+                });
 
-			output = partials.concat(wrappedTemplates);
+            templates = templates.join(grunt.util.normalizelf(options.separator));
 
-			if (output.length < 1) {
-				grunt.log.warn('Destination not written because there was no output.');
-			} else {
-				if (options.min) {
-					output = uglify.minify(output, { fromString: true }).code;
-				}
-				grunt.file.write(f.dest, output);
-				grunt.log.writeln('File "' + f.dest + '" created.');
-			}
+            var wrappedTemplates = prefix + midfix + templates + suffix;
 
-		});
-	});
+            output = partials.concat(wrappedTemplates);
+
+            if (output.length < 1) {
+                grunt.log.warn('Destination not written because there was no output.');
+            } else {
+                if (options.min) {
+                    output = uglify.minify(output, {
+                        fromString: true
+                    }).code;
+                }
+                grunt.file.write(f.dest, output);
+                grunt.log.writeln('File "' + f.dest + '" created.');
+            }
+
+        });
+    });
 };
